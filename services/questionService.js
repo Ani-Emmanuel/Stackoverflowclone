8f923392f1285bfc199d59a3df148d096e5dd308
Emmanuel Ani <mailsforanih@gmail.com>
2020-01-29 17:24:05 +0100

    Added question service, write a query for for handling likes and dislikes updated the models

---
+++ services/questionService.js
@@ -0,0 +1,75 @@
+const { User, Question } = require("../model/model");
+
+module.exports = {
+  //function for get one or all questions
+  getQuestion: async (req, res, next) => {
+    try {
+      query = {};
+      if (req.params.questionId) {
+        const { questionId } = req.params;
+        query._id = questionId;
+      }
+      const askedQuestion = await Question.find(query);
+      res.status(200).json({ payload: { data: askedQuestion } });
+    } catch (error) {
+      next(next);
+    }
+  },
+
+  //function for creating questions
+  askQuestion: async (req, res, next) => {
+    try {
+      const question = new Question(req.body);
+      const createQuestion = await question.save();
+      res.status(201).json({
+        message: "Question created successfully",
+        payload: { data: createQuestion }
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  //function for deleting one question
+  deleteQuestion: async (req, res, next) => {
+    try {
+      const { questionId } = req.params;
+      await Question.findByIdAndDelete({ _id: questionId });
+      res.status(200).json({ message: "Question deleted successfully" });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  //function for updating one question
+  updateQuestion: async (req, res, next) => {
+    try {
+      const { questionId } = req.params;
+      let questionUpdate = {},
+        payload;
+      payload = req.body;
+      if (payload.votequestion == "like") {
+        questionUpdate = await Question.find({ _id: questionId });
+        questionUpdate.up_vote.total = Number(questionUpdate.up_vote.total) + 1;
+        questionUpdate.up_vote.voters.push(userId);
+      }
+      if (payload.votequestion == "dislike") {
+        questionUpdate = await Question.find({ _id: questionId });
+        questionUpdate.down_vote.total =
+          Number(questionUpdate.down_vote.total) - 1;
+        questionUpdate.down_vote.voters.push(userId);
+      }
+      payload.votequestion = questionUpdate;
+      const question = await Question.findByIdAndUpdate(
+        { _id: questionId },
+        payload
+      );
+      res.status(200).json({
+        message: "Question updated successfully",
+        payload: { data: question }
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+};
--- services/userService.js
+++ services/userService.js
@@ -1,59 +1,66 @@
-const { User } = require("../model/model")
-const { hashOperation } = require("../helper/helper")
+const { User } = require("../model/model");
+const { hashOperation } = require("../helper/helper");
 module.exports = {
-    register: async (req, res, next) => {
-        try {
-            //check if user already exist
-            const { email } = req.body;
-            const emailExist = await User.findOne({ email: email })
-            if (emailExist || emailExist.length > 0) {
-                return res.status(400).json({ message: "User with email already exists" })
-            }
+  //function for registering as a user
+  register: async (req, res, next) => {
+    try {
+      //check if user already exist
+      const { email } = req.body;
+      const emailExist = await User.findOne({ email: email });
+      if (emailExist || emailExist.length > 0) {
+        return res
+          .status(400)
+          .json({ message: "User with email already exists" });
+      }
 
-            //created User
-            const user = new User(req.body);
-            const { password } = user;
-            const hashedpass = await hashOperation(password);
-            user.password = hashedpass;
-            user.save();
-            res.status(201).json({ message: "User created successfully" })
-
-        } catch (error) {
-            next(error)
-        }
-    },
-    index: async (req, res, next) => {
-        try {
-            let query = {}
-            //check if req.params exist
-            if (req.params.id) {
-                query = {
-                    _id: req.params.id
-                }
-            }
-            const users = await User.find(query);
-            res.status(200).json({ payload: { users: users } })
-        } catch (error) {
-            next(error)
-        }
-    },
-    updateUser: async (req, res, next) => {
-        try {
-            const { id } = req.params;
-            const user = await User.findByIdAndUpdate(id, req.body, { new: true });
-            res.status(200).json({ message: "User updated successfully" })
-        } catch (error) {
-            next(error)
-        }
-    },
-    deleteUser: async (req, res, next) => {
-        try {
-            const { id } = req.params;
-            await User.findByIdAndDelete(id);
-            res.status(200).json({ message: "User deleted successfully" })
-        } catch (error) {
-            next(error)
-        }
+      //registering User
+      const user = new User(req.body);
+      const { password } = user;
+      const hashedpass = await hashOperation(password);
+      user.password = hashedpass;
+      user.save();
+      res.status(201).json({ message: "User created successfully" });
+    } catch (error) {
+      next(error);
     }
+  },
 
-}
+  //function for getting one or all the users
+  index: async (req, res, next) => {
+    try {
+      let query = {};
+      //check if req.params exist
+      if (req.params.id) {
+        query = {
+          _id: req.params.id
+        };
+      }
+      const users = await User.find(query);
+      res.status(200).json({ payload: { users: users } });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  //function for updateing one users
+  updateUser: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const user = await User.findByIdAndUpdate(id, req.body, { new: true });
+      res.status(200).json({ message: "User updated successfully" });
+    } catch (error) {
+      next(error);
+    }
+  },
+
+  //function for deleting a users
+  deleteUser: async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      await User.findByIdAndDelete(id);
+      res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+      next(error);
+    }
+  }
+};
